e1f71af76bf8bd7f7a2ffa46438fe8181f6cd69b
Mr Esbatu <qorri24j@gmail.com>
2025-01-20 20:19:40 +0700

    add Star Rating and view card review

--- lib/screens/movie_reviews_screen.dart
+++ lib/screens/movie_reviews_screen.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:movie_reviews/widgets/custom_loading.dart';
-
 import '../api_service.dart';
+import '../widgets/custom_loading.dart';
 import 'add_edit_review_screen.dart';
 
 class MovieReviewsScreen extends StatefulWidget {
@@ -15,14 +13,26 @@ class MovieReviewsScreen extends StatefulWidget {
   State<MovieReviewsScreen> createState() => _MovieReviewsScreenState();
 }
 
-class _MovieReviewsScreenState extends State<MovieReviewsScreen> {
+class _MovieReviewsScreenState extends State<MovieReviewsScreen> with SingleTickerProviderStateMixin {
   final _apiService = ApiService();
   List<dynamic> _reviews = [];
   bool _isLoading = false;
+  late AnimationController _likeAnimationController;
+
   @override
   void initState() {
     super.initState();
+    _likeAnimationController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
     _loadReviews();
+  }
+
+  @override
+  void dispose() {
+    _likeAnimationController.dispose();
+    super.dispose();
   }
 
   Future<void> _loadReviews() async {
@@ -37,18 +47,60 @@ class _MovieReviewsScreenState extends State<MovieReviewsScreen> {
     });
   }
 
-  void _deleteReview(String id) async {
-    // Show loading
-    CustomLoading.show();
+  void _toggleLike(String movieId, String username) async {
+    final List<dynamic> likes = await _apiService.getLikeBy(movieId, username);
+    final totalLike = likes.length;
+    final int statusLike = likes.isNotEmpty ? likes.first['status_like'] : 0;
+    if (likes.length > 1) {
+      for (int i = 1; i < likes.length; i++) {
+        await _apiService.deleteLike(likes[i]['_id']);
+      }
+    }
 
-    final success = await _apiService.deleteReview(id);
+    final isLiked = statusLike > 0;
+
+    if (!isLiked) _likeAnimationController.forward(from: 0);
+
+    final success = isLiked && totalLike > 0
+        ? await _apiService.unlike(likes.first['_id'], movieId, username)
+        : !isLiked && totalLike > 0 ? await _apiService.like(likes.first['_id'], movieId, username)
+        : await _apiService.firstLike(movieId, username);
+
     if (success) {
-      // Dismiss loading
-      CustomLoading.dismiss();
+      _loadReviews(); // Refresh data
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Gagal mengubah status suka')),
+      );
+    }
+  }
+
+
+  List<Widget> _buildStarRating(int rating) {
+    const int maxStars = 5;
+    final double normalizedRating = rating / 2;
+
+    List<Widget> stars = [];
+    for (int i = 1; i <= maxStars; i++) {
+      if (i <= normalizedRating) {
+        stars.add(const Icon(Icons.star, color: Colors.yellow, size: 20));
+      } else if (i - 0.5 == normalizedRating) {
+        stars.add(const Icon(Icons.star_half, color: Colors.yellow, size: 20));
+      } else {
+        stars.add(Icon(Icons.star_border, color: Colors.grey.shade400, size: 20));
+      }
+    }
+    return stars;
+  }
+
+  void _deleteReview(String id) async {
+    CustomLoading.show();
+    final success = await _apiService.deleteReview(id);
+    CustomLoading.dismiss();
+
+    if (success) {
       _loadReviews();
     } else {
-      // Dismiss loading
-      CustomLoading.dismiss();
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Gagal menghapus review')),
       );
@@ -76,126 +128,143 @@ class _MovieReviewsScreenState extends State<MovieReviewsScreen> {
           ),
         ],
       ),
-      body: Builder(builder: (context) {
-        // When isLoading
-        if (_isLoading) {
-          return const Center(child: CircularProgressIndicator());
-        }
-
-        // When no review
-        if (_reviews.isEmpty) {
-          return const Center(
-            child: Text(
-              'Belum ada review. Tambahkan sekarang!',
+      body: Stack(
+        children: [
+          if (_reviews.isEmpty && !_isLoading)
+            const Center(
+              child: Text('Belum ada review. Tambahkan sekarang!'),
             ),
-          );
-        }
-
-        // When has review
-        if (!_isLoading) {
-          return ListView.separated(
-            itemCount: _reviews.length,
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            separatorBuilder: (BuildContext context, int index) {
-              return const SizedBox(
-                height: 4,
-              );
-            },
-            itemBuilder: (context, index) {
-              final review = _reviews[index];
-              return InkWell(
-                onTap: () {},
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 4,
+          if (_reviews.isNotEmpty)
+            ListView.builder(
+              itemCount: _reviews.length,
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              itemBuilder: (context, index) {
+                final review = _reviews[index];
+                return Card(
+                  margin: const EdgeInsets.only(bottom: 12),
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Column(
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Image.memory(
-                            base64Decode(review['image']),
-                            fit: BoxFit.cover,
-                            width: 100,
-                            height: 150,
-                            errorBuilder: (_, __, ___) {
-                              return Container(
-                                width: 100,
-                                height: 150,
-                                color: Colors.grey,
-                              );
-                            },
-                          ),
-                          const SizedBox(
-                            width: 10.0,
-                          ),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  review['title'],
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  "${review['rating']} / 10",
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                Text(
-                                  "${review['comment']}",
-                                  maxLines: 3,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ],
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.memory(
+                                base64Decode(review['image']),
+                                width: MediaQuery.of(context).size.width * 0.25,
+                                height: 120,
+                                fit: BoxFit.cover,
+                                errorBuilder: (_, __, ___) {
+                                  return Container(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.25,
+                                    height: 120,
+                                    color: Colors.grey.shade300,
+                                    child: const Icon(
+                                      Icons.image,
+                                      size: 40,
+                                      color: Colors.grey,
+                                    ),
+                                  );
+                                },
+                              ),
                             ),
-                          )
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.thumb_up_outlined),
-                            onPressed: () {},
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.edit),
-                            onPressed: () async {
-                              final result = await Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => AddEditReviewScreen(
-                                    username: widget.username,
-                                    review: review,
+                            const SizedBox(width: 10),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    review['title'],
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                              );
-                              if (result == true) _loadReviews();
-                            },
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () => _deleteReview(review['_id']),
-                          ),
-                        ],
-                      ),
-                    ],
+                                  const SizedBox(height: 6),
+                                  Row(
+                                    children: _buildStarRating(
+                                        review['rating']),
+                                  ),
+                                  const SizedBox(height: 6),
+                                  Text(
+                                    review['comment'],
+                                    maxLines: 3,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(fontSize: 14),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            GestureDetector(
+                              onTap: () => _toggleLike(review['_id'],review['username']),
+                              child: Row(
+                                children: [
+                                  ScaleTransition(
+                                    scale: Tween(begin: 1.0, end: 1.2)
+                                        .animate(CurvedAnimation(
+                                        parent: _likeAnimationController,
+                                        curve: Curves.easeOut)),
+                                    child: Icon(
+                                      review['username'] == widget.username
+                                          ? Icons.thumb_up
+                                          : Icons.thumb_up_outlined,
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 4),
+                                ],
+                              ),
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.edit),
+                              onPressed: () async {
+                                final result = await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        AddEditReviewScreen(
+                                          username: widget.username,
+                                          review: review,
+                                        ),
+                                  ),
+                                );
+                                if (result == true) _loadReviews();
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () => _deleteReview(review['_id']),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              );
-            },
-          );
-        }
-        return const SizedBox();
-      }),
+                );
+              },
+            ),
+          if (_isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
